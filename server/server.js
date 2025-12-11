3d7a05de335f59029729183da4057bd8183df643
Rahman <rahmanhasibu2@gmail.com>
2025-12-12 00:06:11 +0530

    correction in sernver file

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.set('io', io);
 
 console.log("Connecting to MongoDB...");
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/swasthya_setu')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log('MongoDB Connection Error:', err));
 
@@ -97,11 +97,11 @@ io.on('connection', (socket) => {
         console.log('User Disconnected', socket.id);
     });
 });
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    httpServer.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
 
-export default server;
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
+export default httpServer;
