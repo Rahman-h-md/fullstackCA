170c8fd77dda8c42691955b7781c586ae14796ba
Rahman <rahmanhasibu2@gmail.com>
2025-12-14 03:02:02 +0530

    Add TURN credentials

--- client/src/hooks/useWebRTC.js
+++ client/src/hooks/useWebRTC.js
@@ -19,15 +19,22 @@ const useWebRTC = (roomId, isInitiator = false) => {
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
-            // OpenRelay with multiple transports for better connectivity
+            { urls: 'stun:global.stun.twilio.com:3478' },
+            // Robust Twilio TURN servers for reliable connection
             {
-                urls: [
-                    'turn:openrelay.metered.ca:80',
-                    'turn:openrelay.metered.ca:443',
-                    'turn:openrelay.metered.ca:443?transport=tcp'
-                ],
-                username: 'openrelayproject',
-                credential: 'openrelayproject'
+                urls: 'turn:global.turn.twilio.com:3478?transport=udp',
+                username: 'dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269',
+                credential: 'tE2DajzSJwnsSbc123'
+            },
+            {
+                urls: 'turn:global.turn.twilio.com:3478?transport=tcp',
+                username: 'dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269',
+                credential: 'tE2DajzSJwnsSbc123'
+            },
+            {
+                urls: 'turn:global.turn.twilio.com:443?transport=tcp',
+                username: 'dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269',
+                credential: 'tE2DajzSJwnsSbc123'
             }
         ]
     };
