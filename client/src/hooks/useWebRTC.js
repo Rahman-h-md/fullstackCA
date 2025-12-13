34ef3e34a46f75a4eb73178ff40d35b459d0402d
Rahman <rahmanhasibu2@gmail.com>
2025-12-14 02:30:22 +0530

    Update TURN config- TCP

--- client/src/hooks/useWebRTC.js
+++ client/src/hooks/useWebRTC.js
@@ -19,14 +19,13 @@ const useWebRTC = (roomId, isInitiator = false) => {
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
-            // Free TURN server for NAT traversal (required for different networks)
+            // OpenRelay with multiple transports for better connectivity
             {
-                urls: 'turn:openrelay.metered.ca:80',
-                username: 'openrelayproject',
-                credential: 'openrelayproject'
-            },
-            {
-                urls: 'turn:openrelay.metered.ca:443',
+                urls: [
+                    'turn:openrelay.metered.ca:80',
+                    'turn:openrelay.metered.ca:443',
+                    'turn:openrelay.metered.ca:443?transport=tcp'
+                ],
                 username: 'openrelayproject',
                 credential: 'openrelayproject'
             }
